143cd4c28ba2596e20849b263053d7ee68b1b294
Матвей <aniproject59@gmail.com>
2026-03-21 16:28:58 +0500

    refactor: remove WPF Brush from ViewModel, expose hex strings with converter

--- BakhmatovCalculatorApp/ViewModels/MainViewModel.cs
+++ BakhmatovCalculatorApp/ViewModels/MainViewModel.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
-using System.Windows.Media;
 using BakhmatovCalculatorLib.Calculators;
 using BakhmatovCalculatorLib.Models;
 using BakhmatovCalculatorLib.Services;
@@ -13,33 +12,28 @@ namespace BakhmatovCalculatorApp.ViewModels;
 
 public sealed class MainViewModel : INotifyPropertyChanged
 {
-    private enum UiLanguage
-    {
-        Ru,
-        En
-    }
+    private enum UiLanguage { Ru, En }
 
     private readonly CalculatorEngine _engine;
+    private readonly RelayCommand _evaluateCommand;
     private UiLanguage _currentLanguage;
 
     private string _expression = string.Empty;
     private string _resultText = string.Empty;
 
-    private Brush _pageBackground = Brushes.Black;
-    private Brush _panelBackground = Brushes.Transparent;
+    private string _pageBackground = "#000000";
+    private string _panelBackground = "#000000";
+    private string _keyBackground = "#000000";
+    private string _hoverKeyBackground = "#000000";
+    private string _pressedKeyBackground = "#000000";
+    private string _keyBorderBrush = "#000000";
+    private string _keyForeground = "#000000";
+    private string _accentKeyBackground = "#1E90FF";
+    private string _hoverAccentKeyBackground = "#1E90FF";
+    private string _pressedAccentKeyBackground = "#1E90FF";
+    private string _accentKeyForeground = "#FFFFFF";
+    private string _secondaryText = "#888888";
 
-    private Brush _keyBackground = Brushes.Transparent;
-    private Brush _hoverKeyBackground = Brushes.Transparent;
-    private Brush _pressedKeyBackground = Brushes.Transparent;
-    private Brush _keyBorderBrush = Brushes.Transparent;
-    private Brush _keyForeground = Brushes.Black;
-
-    private Brush _accentKeyBackground = Brushes.DeepSkyBlue;
-    private Brush _hoverAccentKeyBackground = Brushes.DeepSkyBlue;
-    private Brush _pressedAccentKeyBackground = Brushes.DeepSkyBlue;
-    private Brush _accentKeyForeground = Brushes.White;
-
-    private Brush _secondaryText = Brushes.Gray;
     private string _windowTitle = string.Empty;
     private string _themeButtonText = string.Empty;
     private string _languageButtonText = string.Empty;
@@ -49,7 +43,6 @@ public sealed class MainViewModel : INotifyPropertyChanged
 
     public MainViewModel()
     {
-        // Let the library decide default persistence locations.
         _engine = new CalculatorEngine();
 
         HistoryItems = new ObservableCollection<CalculationHistoryItem>();
@@ -58,25 +51,22 @@ public sealed class MainViewModel : INotifyPropertyChanged
         ClearAllCommand = new RelayCommand(_ => ClearAll());
         ClearEntryCommand = new RelayCommand(_ => ClearEntry());
         BackspaceCommand = new RelayCommand(_ => Backspace());
-
-        _evaluateCommand = new RelayCommand(_ => EvaluateAndCommit(), _ => !string.IsNullOrWhiteSpace(Expression));
-        EvaluateCommand = _evaluateCommand;
-
         ToggleThemeCommand = new RelayCommand(_ => ToggleTheme());
         ToggleLanguageCommand = new RelayCommand(_ => ToggleLanguage());
         UseHistoryCommand = new RelayCommand(p => UseHistory(p as CalculationHistoryItem));
 
+        _evaluateCommand = new RelayCommand(_ => EvaluateAndCommit(), _ => !string.IsNullOrWhiteSpace(Expression));
+        EvaluateCommand = _evaluateCommand;
+
         _currentLanguage = CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "ru"
-            ? UiLanguage.Ru
-            : UiLanguage.En;
+            ? UiLanguage.Ru : UiLanguage.En;
 
         ApplyLocalization();
-        UpdateThemeBrushes();
+        UpdateThemeColors();
         RefreshHistory();
     }
 
     public ObservableCollection<CalculationHistoryItem> HistoryItems { get; }
-
     public event PropertyChangedEventHandler? PropertyChanged;
 
     public string Expression
@@ -95,77 +85,25 @@ public sealed class MainViewModel : INotifyPropertyChanged
         set => SetProperty(ref _resultText, value ?? string.Empty);
     }
 
-    public Brush PageBackground
-    {
-        get => _pageBackground;
-        private set => SetProperty(ref _pageBackground, value);
-    }
+    public string PageBackground { get => _pageBackground; private set => SetProperty(ref _pageBackground, value); }
+    public string PanelBackground { get => _panelBackground; private set => SetProperty(ref _panelBackground, value); }
+    public string KeyBackground { get => _keyBackground; private set => SetProperty(ref _keyBackground, value); }
+    public string HoverKeyBackground { get => _hoverKeyBackground; private set => SetProperty(ref _hoverKeyBackground, value); }
+    public string PressedKeyBackground { get => _pressedKeyBackground; private set => SetProperty(ref _pressedKeyBackground, value); }
+    public string KeyBorderBrush { get => _keyBorderBrush; private set => SetProperty(ref _keyBorderBrush, value); }
+    public string KeyForeground { get => _keyForeground; private set => SetProperty(ref _keyForeground, value); }
+    public string AccentKeyBackground { get => _accentKeyBackground; private set => SetProperty(ref _accentKeyBackground, value); }
+    public string HoverAccentKeyBackground { get => _hoverAccentKeyBackground; private set => SetProperty(ref _hoverAccentKeyBackground, value); }
+    public string PressedAccentKeyBackground { get => _pressedAccentKeyBackground; private set => SetProperty(ref _pressedAccentKeyBackground, value); }
+    public string AccentKeyForeground { get => _accentKeyForeground; private set => SetProperty(ref _accentKeyForeground, value); }
+    public string SecondaryText { get => _secondaryText; private set => SetProperty(ref _secondaryText, value); }
 
-    public Brush PanelBackground
-    {
-        get => _panelBackground;
-        private set => SetProperty(ref _panelBackground, value);
-    }
-
-    public Brush KeyBackground
-    {
-        get => _keyBackground;
-        private set => SetProperty(ref _keyBackground, value);
-    }
-
-    public Brush HoverKeyBackground
-    {
-        get => _hoverKeyBackground;
-        private set => SetProperty(ref _hoverKeyBackground, value);
-    }
-
-    public Brush PressedKeyBackground
-    {
-        get => _pressedKeyBackground;
-        private set => SetProperty(ref _pressedKeyBackground, value);
-    }
-
-    public Brush KeyBorderBrush
-    {
-        get => _keyBorderBrush;
-        private set => SetProperty(ref _keyBorderBrush, value);
-    }
-
-    public Brush KeyForeground
-    {
-        get => _keyForeground;
-        private set => SetProperty(ref _keyForeground, value);
-    }
-
-    public Brush AccentKeyBackground
-    {
-        get => _accentKeyBackground;
-        private set => SetProperty(ref _accentKeyBackground, value);
-    }
-
-    public Brush HoverAccentKeyBackground
-    {
-        get => _hoverAccentKeyBackground;
-        private set => SetProperty(ref _hoverAccentKeyBackground, value);
-    }
-
-    public Brush PressedAccentKeyBackground
-    {
-        get => _pressedAccentKeyBackground;
-        private set => SetProperty(ref _pressedAccentKeyBackground, value);
-    }
-
-    public Brush AccentKeyForeground
-    {
-        get => _accentKeyForeground;
-        private set => SetProperty(ref _accentKeyForeground, value);
-    }
-
-    public Brush SecondaryText
-    {
-        get => _secondaryText;
-        private set => SetProperty(ref _secondaryText, value);
-    }
+    public string WindowTitle { get => _windowTitle; private set => SetProperty(ref _windowTitle, value); }
+    public string ThemeButtonText { get => _themeButtonText; private set => SetProperty(ref _themeButtonText, value); }
+    public string LanguageButtonText { get => _languageButtonText; private set => SetProperty(ref _languageButtonText, value); }
+    public string HistoryTitleText { get => _historyTitleText; private set => SetProperty(ref _historyTitleText, value); }
+    public string ExpressionLabelText { get => _expressionLabelText; private set => SetProperty(ref _expressionLabelText, value); }
+    public string ResultLabelText { get => _resultLabelText; private set => SetProperty(ref _resultLabelText, value); }
 
     public ICommand AppendCommand { get; }
     public ICommand ClearAllCommand { get; }
@@ -175,53 +113,19 @@ public sealed class MainViewModel : INotifyPropertyChanged
     public ICommand ToggleThemeCommand { get; }
     public ICommand ToggleLanguageCommand { get; }
     public ICommand UseHistoryCommand { get; }
-    public string WindowTitle
-    {
-        get => _windowTitle;
-        private set => SetProperty(ref _windowTitle, value);
-    }
-
-    public string ThemeButtonText
-    {
-        get => _themeButtonText;
-        private set => SetProperty(ref _themeButtonText, value);
-    }
-
-    public string LanguageButtonText
-    {
-        get => _languageButtonText;
-        private set => SetProperty(ref _languageButtonText, value);
-    }
-
-    public string HistoryTitleText
-    {
-        get => _historyTitleText;
-        private set => SetProperty(ref _historyTitleText, value);
-    }
-
-    public string ExpressionLabelText
-    {
-        get => _expressionLabelText;
-        private set => SetProperty(ref _expressionLabelText, value);
-    }
-
-    public string ResultLabelText
-    {
-        get => _resultLabelText;
-        private set => SetProperty(ref _resultLabelText, value);
-    }
-
-
-    private readonly RelayCommand _evaluateCommand;
 
     public void Persist() => _engine.SaveAll();
 
     public void AppendToken(string token)
     {
-        if (string.IsNullOrEmpty(token))
-            return;
+        if (!string.IsNullOrEmpty(token))
+            Expression += token;
+    }
 
-        Expression += token;
+    public void Backspace()
+    {
+        if (!string.IsNullOrEmpty(Expression))
+            Expression = Expression[..^1];
     }
 
     private void EvaluateAndCommit()
@@ -241,14 +145,6 @@ public sealed class MainViewModel : INotifyPropertyChanged
         }
     }
 
-    public void Backspace()
-    {
-        if (string.IsNullOrEmpty(Expression))
-            return;
-
-        Expression = Expression[..^1];
-    }
-
     private void ClearAll()
     {
         Expression = string.Empty;
@@ -260,10 +156,7 @@ public sealed class MainViewModel : INotifyPropertyChanged
         if (string.IsNullOrWhiteSpace(Expression))
             return;
 
-        // Remove last "number segment" at the end of the expression.
-        // Example: "3*-2" -> remove "-2" (with its unary '-') but "5-2" -> remove "2".
         var expr = Expression;
-
         int end = expr.Length - 1;
 
         while (end >= 0 && (char.IsDigit(expr[end]) || expr[end] == '.' || expr[end] == ','))
@@ -271,13 +164,10 @@ public sealed class MainViewModel : INotifyPropertyChanged
 
         var removeStart = end + 1;
 
-        // If the number had a unary '-' sign, remove it too.
         if (removeStart > 0 && expr[removeStart - 1] == '-')
         {
             if (removeStart - 1 == 0)
-            {
                 removeStart--;
-            }
             else
             {
                 char prev = expr[removeStart - 2];
@@ -293,9 +183,8 @@ public sealed class MainViewModel : INotifyPropertyChanged
     private void ToggleTheme()
     {
         var current = _engine.GetThemeSettings();
-        var next = new ThemeSettings(!current.IsDarkTheme, current.AccentColor);
-        _engine.UpdateThemeSettings(next);
-        UpdateThemeBrushes();
+        _engine.UpdateThemeSettings(new ThemeSettings(!current.IsDarkTheme, current.AccentColor));
+        UpdateThemeColors();
     }
 
     private void ToggleLanguage()
@@ -306,11 +195,9 @@ public sealed class MainViewModel : INotifyPropertyChanged
 
     private void UseHistory(CalculationHistoryItem? item)
     {
-        if (item is null)
-            return;
+        if (item is null) return;
 
         Expression = item.Expression;
-
         try
         {
             var result = _engine.Evaluate(Expression);
@@ -325,64 +212,43 @@ public sealed class MainViewModel : INotifyPropertyChanged
     private void RefreshHistory()
     {
         HistoryItems.Clear();
-
-        // Show newest first.
         var history = _engine.GetHistory();
         for (int i = history.Count - 1; i >= 0; i--)
             HistoryItems.Add(history[i]);
     }
 
-    private void UpdateThemeBrushes()
+    private void UpdateThemeColors()
     {
         var settings = _engine.GetThemeSettings();
-
-        Color accentColor;
-        try
-        {
-            var converted = ColorConverter.ConvertFromString(settings.AccentColor);
-            accentColor = converted is Color c ? c : Color.FromRgb(30, 144, 255);
-        }
-        catch
-        {
-            accentColor = Color.FromRgb(30, 144, 255);
-        }
+        var accent = settings.AccentColor;
 
         if (settings.IsDarkTheme)
         {
-            PageBackground = new SolidColorBrush(Color.FromRgb(15, 17, 23));
-            PanelBackground = new SolidColorBrush(Color.FromRgb(27, 29, 38));
-
-            KeyBackground = new SolidColorBrush(Color.FromRgb(33, 38, 51));
-            HoverKeyBackground = new SolidColorBrush(Color.FromRgb(44, 49, 65));
-            PressedKeyBackground = new SolidColorBrush(Color.FromRgb(24, 28, 40));
-            KeyBorderBrush = new SolidColorBrush(Color.FromRgb(55, 61, 78));
-            KeyForeground = new SolidColorBrush(Color.FromRgb(230, 234, 242));
-
-            AccentKeyBackground = new SolidColorBrush(accentColor);
-            HoverAccentKeyBackground = new SolidColorBrush(AdjustColor(accentColor, 1.12));
-            PressedAccentKeyBackground = new SolidColorBrush(AdjustColor(accentColor, 0.86));
-            AccentKeyForeground = Brushes.White;
-
-            SecondaryText = new SolidColorBrush(Color.FromRgb(168, 176, 192));
+            PageBackground = "#0F1117";
+            PanelBackground = "#1B1D26";
+            KeyBackground = "#212633";
+            HoverKeyBackground = "#2C3141";
+            PressedKeyBackground = "#181C28";
+            KeyBorderBrush = "#373D4E";
+            KeyForeground = "#E6EAF2";
+            SecondaryText = "#A8B0C0";
         }
         else
         {
-            PageBackground = new SolidColorBrush(Color.FromRgb(218, 222, 230));
-            PanelBackground = new SolidColorBrush(Color.FromRgb(228, 232, 240));
-
-            KeyBackground = new SolidColorBrush(Color.FromRgb(210, 215, 225));
-            HoverKeyBackground = new SolidColorBrush(Color.FromRgb(200, 205, 216));
-            PressedKeyBackground = new SolidColorBrush(Color.FromRgb(188, 194, 206));
-            KeyBorderBrush = new SolidColorBrush(Color.FromRgb(190, 196, 210));
-            KeyForeground = new SolidColorBrush(Color.FromRgb(22, 28, 42));
-
-            AccentKeyBackground = new SolidColorBrush(accentColor);
-            HoverAccentKeyBackground = new SolidColorBrush(AdjustColor(accentColor, 1.08));
-            PressedAccentKeyBackground = new SolidColorBrush(AdjustColor(accentColor, 0.9));
-            AccentKeyForeground = Brushes.White;
-
-            SecondaryText = new SolidColorBrush(Color.FromRgb(80, 90, 112));
+            PageBackground = "#DADEE6";
+            PanelBackground = "#E4E8F0";
+            KeyBackground = "#D2D7E1";
+            HoverKeyBackground = "#C8CDD8";
+            PressedKeyBackground = "#BCBECE";
+            KeyBorderBrush = "#BEC4D2";
+            KeyForeground = "#161C2A";
+            SecondaryText = "#505A70";
         }
+
+        AccentKeyBackground = accent;
+        HoverAccentKeyBackground = AdjustHex(accent, settings.IsDarkTheme ? 1.12 : 1.08);
+        PressedAccentKeyBackground = AdjustHex(accent, settings.IsDarkTheme ? 0.86 : 0.90);
+        AccentKeyForeground = "#FFFFFF";
     }
 
     private void ApplyLocalization()
@@ -406,24 +272,26 @@ public sealed class MainViewModel : INotifyPropertyChanged
         ResultLabelText = "Result";
     }
 
-    private static Color AdjustColor(Color color, double factor)
+    private static string AdjustHex(string hex, double factor)
     {
-        byte Scale(byte channel)
-        {
-            var value = (int)(channel * factor);
-            if (value < 0) return 0;
-            if (value > 255) return 255;
-            return (byte)value;
-        }
+        var (r, g, b) = ParseHex(hex);
+        byte Scale(byte ch) => (byte)Math.Clamp((int)(ch * factor), 0, 255);
+        return $"#{Scale(r):X2}{Scale(g):X2}{Scale(b):X2}";
+    }
 
-        return Color.FromRgb(Scale(color.R), Scale(color.G), Scale(color.B));
+    private static (byte r, byte g, byte b) ParseHex(string hex)
+    {
+        hex = hex.TrimStart('#');
+        return (
+            Convert.ToByte(hex[0..2], 16),
+            Convert.ToByte(hex[2..4], 16),
+            Convert.ToByte(hex[4..6], 16)
+        );
     }
 
     private bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
     {
-        if (Equals(field, value))
-            return false;
-
+        if (Equals(field, value)) return false;
         field = value;
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         return true;
